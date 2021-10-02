38ff78307bfd6983fa1257bc3439672ca0d96822
ELIAS ZACARIAS LOLEGI <69213659+EliasLolegi@users.noreply.github.com>
2021-10-02 05:31:27 -0300

    Create Basico Aula01.js

---
+++ Basico Aula01.js
@@ -0,0 +1,25 @@
+
+
+//Aula 01
+var nome = "Elias Zacarias Lolegi";
+var frase= "japão é o melhor time do mundo";
+var n1 = 5;
+var n2 = 3;
+
+//var idade = 31;
+//var idade2 = 10;
+
+//alert(nome +" tem"+ idade + " anos" );
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(n1 * n2);
+//console.log(idade + idade2);
+console.log(frase.replace.toUppercase("japão","Brasil")) //renomeia japão para Brasil
+ //renomeia frase no console (cliente)
+
+console.log(frase.toUpperCase()) //deixa o elemento em maiuscula
+console.log(frase.toLowerCase()) //deixa o elemento em minuscula
+
+//console.log(frase.replace("japão","Brasil"))
+//alert(frase.replace("japão","Brasil"));
